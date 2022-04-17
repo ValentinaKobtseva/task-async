1543bbc4371b03b73b70c3c8608389ab629ef366
Валентина Кобцева <kvaaa0601@gmail.com>
2022-04-18 02:11:10 +0700

    Задание ассинхронность

--- src/Async.ts
+++ src/Async.ts
@@ -3,6 +3,13 @@
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    let n: number;
+    let pr = new Promise<number>((resolve, reject) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
+    return pr;
 }
 
 /*
@@ -11,19 +18,14 @@ export function mock(ms: number): Promise<number> {
 export function getData(): Promise<number[]> {
     const result: number[] = [];
 
-    return mock(100)
-        .then((data1) => {
+    return Promise.all([mock(100), mock(200), mock(300)]).then(
+        ([data1, data2, data3]) => {
             result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
             result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
             result.push(data3);
             return result;
-        });
+        },
+    );
 }
 
 /*
@@ -31,9 +33,9 @@ export function getData(): Promise<number[]> {
 завершенный с ошибкой Promise и возвращал текст ошибки.
  */
 export async function catchException(): Promise<string | undefined> {
-    try {
-        Promise.reject(new Error('my error'));
-    } catch (err) {
-        return err.message;
-    }
+    return Promise.reject(new Error('my error')).catch((err) => {
+        if (err instanceof Error) {
+            return err.message;
+        }
+    });
 }
